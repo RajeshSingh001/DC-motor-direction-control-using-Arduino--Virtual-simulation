e8903477ffee8dcc1568e28ef37172668b728f13
Dr. Rajesh Singh <75157838+RajeshSingh001@users.noreply.github.com>
2020-11-28 19:34:42 +0530

    Arduino IDE code and Proteus model

---
+++ motor_dc.ino
@@ -0,0 +1,18 @@
+void setup()
+{
+pinMode(7, OUTPUT);
+pinMode(6, OUTPUT);
+}
+
+void loop()
+{
+digitalWrite(7, HIGH);// clockwise
+digitalWrite(6, LOW);
+delay(2000);
+digitalWrite(7, LOW); // Anticlockwise
+digitalWrite(6, HIGH);
+delay(2000);
+digitalWrite(7, LOW); // stop
+digitalWrite(6, LOW);
+delay(2000);
+}
